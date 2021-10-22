57155776af0056717f8c5c074cb59903deb1b7e3
Onvyr <nikodem.pilachowski@gmail.com>
2021-10-22 11:20:51 +0200

    Add project files.

---
+++ Tutorial/Cos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace InterFaceDemo
+{
+    interface IOne
+    {
+        void ONE();
+    }
+    interface ITwo
+    {
+        void TWO();
+    }
+    interface IThree : IOne
+    {
+        void THREE();
+    }
+    interface IFour
+    {
+        void FOUR();
+    }
+    interface IFive : IThree
+    {
+        void FIVE();
+    }
+    interface IEVEN : ITwo, IFour { }
+    class ODDEVEN : IEVEN, IFive
+    {
+        public void ONE()
+        {
+            Console.WriteLine("This is ONE");
+        }
+        public void TWO()
+        {
+            Console.WriteLine("This is TWO");
+        }
+        public void THREE()
+        {
+            Console.WriteLine("This is THERE");
+        }
+        public void FOUR()
+        {
+            Console.WriteLine("This is FOUR");
+        }
+        public void FIVE()
+        {
+            Console.WriteLine("This is FIVE");
+        }
+    }
+}
---
+++ Tutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace InterFaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFive obj1 = new ODDEVEN();
+            obj1.ONE();
+            obj1.THREE();
+            obj1.FIVE();
+            IEVEN obj2 = new ODDEVEN();
+            obj2.TWO();
+            obj2.FOUR();
+        }
+    }
+}
